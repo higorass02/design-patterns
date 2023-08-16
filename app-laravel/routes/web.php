26403389de2ead0f89fbae15d0f500693afa92f0
Higor Santos <higoraugusto120@gmail.com>
2023-08-16 00:00:34 -0300

    push singleton exemplo

--- app-laravel/routes/web.php
+++ app-laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DesignPatterns\Singleton\SingletonController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/singleton')->group( function () { 
+    Route::get('', [SingletonController::class, 'index'])->name('singleton.index');
 });
